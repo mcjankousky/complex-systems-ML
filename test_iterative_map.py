8f04bc964d7e55b99fb7ad15cd67559900f2d821
Matthew Jankousky <matthewcjankousky@gmail.com>
2026-05-19 20:08:53 -0600

    parametrized simple quadratic test

--- test_iterative_map.py
+++ test_iterative_map.py
@@ -6,8 +6,18 @@ Created on Mon May 18 20:53:11 2026
 """
 
 from IterativeMap import IterativeMap
+import pytest
 
+@pytest.fixture
+def iterative_map():
+    return IterativeMap()
 
-def test_quadratic_map_max():
-    obj = IterativeMap()
-    assert obj.quadratic_map(0.5,4)==1
+@pytest.mark.parametrize("state_start, scale, expected_value", [
+    (0,3,0),
+    (0.5,4,1),
+    (0.5,2,0.5)])
+
+def test_quadratic_map_1_step(iterative_map,state_start,scale,expected_value):
+    assert iterative_map.quadratic_map(state_start,scale) == expected_value
+
+    
